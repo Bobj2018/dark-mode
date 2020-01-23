21ffbe20e0219f92a84a8ad43fdb8ae39c55863e
Joshua Rieth <joshua.m.rieth@gmail.com>
2020-01-22 19:57:05 -0500

    completed MVP

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,5 +1,14 @@
 import { useLocalStorage } from './useLocalStorage';
+import { useEffect } from 'react';
 
 export const useDarkMode = () => {
-	const [value, setValue] = useLocalStorage('true');
+	const [darkMode, setDarkMode] = useLocalStorage('darkMode');
+
+	useEffect(() => {
+		darkMode
+			? document.body.classList.add('dark-mode')
+			: document.body.classList.remove('dark-mode');
+	}, [darkMode]);
+
+	return [darkMode, setDarkMode];
 };
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -3,11 +3,12 @@ import { useState } from 'react';
 export const useLocalStorage = (key, initialValue) => {
 	const [storedValue, setStoredValue] = useState(() => {
 		const item = window.localStorage.getItem(key);
-        return item ? JSON.parse(item) : initialValue;
-        const setValue => {
-    setStoredValue(value);
-    window.localStorage.setItem(key, JSON.stringify(value));
-        }
+		return item ? JSON.parse(item) : initialValue;
 	});
-	return [storedValue];
+
+	const setValue = value => {
+		setStoredValue(value);
+		window.localStorage.setItem(key, JSON.stringify(value));
+	};
+	return [storedValue, setValue];
 };
